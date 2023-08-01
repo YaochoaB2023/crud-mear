5f18a762445bfa922f1b6281644a4151cc6a62d7
unknown <YaochoaB2023>
2023-08-01 18:47:28 -0500

    actualice las rutas para que tengan validacion de acceso con el token

--- servidor/src/routes/task.routes.js
+++ servidor/src/routes/task.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getTasks, getTask, createTask, updateTask, deleteTask } from "../controllers/task.controller.js";
+import { requireAuth } from "../middlewares/tokenValidation.js";
 
 
 const router = Router();
 
-router.get('/tasks', getTasks)
-router.get('/task/:id', getTask)
-router.post('/task', createTask)
-router.put('/task/:id', updateTask)
-router.delete('/task/:id', deleteTask)
+router.get('/tasks',requireAuth, getTasks)
+router.get('/task/:id',requireAuth, getTask)
+router.post('/task',requireAuth, createTask)
+router.put('/task/:id',requireAuth, updateTask)
+router.delete('/task/:id',requireAuth, deleteTask)
 
 export default router;
