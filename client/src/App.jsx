b219e419084bbdf9cecffefbc0a74f9a2b1c0bda
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-08-04 20:22:32 -0500

    agregue las rutas a la pagina

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,13 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path= '/' element = { <h1>home page</h1> } />
-          <Route path= '/login' element = { <LoginPage/> } />
-          <Route path= '/register' element = { <RegisterPage/> } />
-          <Route path= '/tasks' element = { <h1>Tasks page</h1> } />
-          <Route path= '/add-task' element = { <h1>New task </h1> } />
-          <Route path= '/tasks/:id' element = { <h1>Update task</h1> } />
-          <Route path= '/profile' element = { <h1>Profile</h1> } />
+          <Route path = '/' element = { <h1>home page</h1> } />
+          <Route path = '/login' element = { <LoginPage/> } />
+          <Route path = '/register' element = { <RegisterPage/> } />
+          <Route path = '/tasks' element = { <h1>Tasks page</h1> } />
+          <Route path = '/add-task' element = { <h1>New task </h1> } />
+          <Route path = '/tasks/:id' element = { <h1>Update task</h1> } />
+          <Route path = '/profile' element = { <h1>Profile</h1> } />
         </Routes>
       </BrowserRouter>
     </>
