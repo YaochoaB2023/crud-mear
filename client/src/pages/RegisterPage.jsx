e2929705a915ed6cc4c3e250084428c03659a937
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-08-04 20:26:50 -0500

    cree la pagina de resgistro funcional

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,8 +1,36 @@
+import { useForm } from 'react-hook-form';
+import '../css/register.css';
+import { registerRequest } from '../api/auth';
 
 function RegisterPage() {
+
+  const { register, handleSubmit } = useForm();
+
   return (
     <div>
-        RegisterPage
+      <div className="caja">  
+        <div className="centrar">
+          <form onSubmit={handleSubmit( async(values) => {
+            console.log(values);
+            const res = await registerRequest(values);
+            console.log(res);
+          })}>
+            <div className="mb-6">
+              <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your user</label>
+              <input type="text" {...register("username", {require: true})} id="username" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="username" required/>
+            </div>
+            <div className="mb-6">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input type="email" {...register("email", {require: true})} id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="email" required/>
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+              <input type="password" {...register("password", {require: true})} id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required/>
+            </div>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
