c984084b87f158b1b87efdf06eacd6d92ecbc17f
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-09-04 21:00:19 -0500

    Agrega configuración CORS para permitir origen http://localhost:5173 con credenciales

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -6,7 +6,10 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+app.use( cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}) )
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser()); // para manejar y acceder de forma mas facil  a las cookies
